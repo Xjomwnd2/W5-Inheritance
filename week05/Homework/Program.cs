6ba2bc60dc6f08c84765c120b9a83145ee5ac029
YourGitHubUsername <jmwaura@byupathway.edu>
2025-04-05 12:13:06 +0300

    COMMIT

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,85 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Test the base Assignment class
+        Console.WriteLine("Testing the base Assignment class:");
+        Assignment simpleAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(simpleAssignment.GetSummary());
+        Console.WriteLine();
+
+        // Test the MathAssignment class
+        Console.WriteLine("Testing the MathAssignment class:");
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+
+        // Test the WritingAssignment class
+        Console.WriteLine("Testing the WritingAssignment class:");
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+    }
+}
+
+// Base Assignment class
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+
+    // Protected getter for the student name to be used by derived classes
+    protected string GetStudentName()
+    {
+        return _studentName;
+    }
+}
+
+// MathAssignment class that inherits from Assignment
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems)
+        : base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+}
+
+// WritingAssignment class that inherits from Assignment
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        // Using the protected getter method from the base class
+        string studentName = GetStudentName();
+        return $"{_title} by {studentName}";
     }
 }
